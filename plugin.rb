ed1175bd08f8ff7c5a1ee6354f1233ac8a719b91
scavin <scavina@gmail.com>
2025-11-20 09:48:22 +0800

    fix: ensure plugin metadata loads

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
-# frozen_string_literal: true
-
 # name: meta-appinn-uploads
 # about: Meta Appinn Uploads replaces Discourse attachments with uploads to h1.appinn.me and Markdown links.
 # version: 0.1.0
 # authors: Appinn Community
 # url: https://github.com/scavin/Meta-Appinn-Uploads
+
+# frozen_string_literal: true
